7297f07f4d7695e7f8938b0143a5326264016585
yafanisonya <yafanison@gmail.com>
2020-07-24 21:49:45 +0800

    Directives

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -49,44 +49,33 @@
 <!--}-->
 <!--</script>-->
 
-
-<!--<style>-->
-<!--</style>-->
 <template>
-  <div id="app">
+  <div>
     <a-tabs>
-      <a-tab-pane key="clock" tab="时钟">
-        <button @click="destroyClock = !destroyClock">
-          {{ destroyClock ? "加载时钟" : "销毁时钟"}}
-        </button>
-        <Clock v-if="!destroyClock"/>
+      <a-tab-pane key="Directives" tab="内置指令">
+        <Directives/>
       </a-tab-pane>
-      <a-tab-pane key="Functional" tab="函数式组件">
-        <Functional :name="name"/>
-        <TempVar
-          :var1="`hello ${name}`"
-          :var2="destroyClock ? 'hello vue' : 'hello world'">
-          <template v-slot="{var1,var2}">{{ var1 }} {{ var2 }}</template>
-        </TempVar>
+      <a-tab-pane key="CustomerDirectives" tab="自定义指令">
+        <CustomerDirectives/>
       </a-tab-pane>
     </a-tabs>
   </div>
 </template>
-<script>
-  import Clock from "../src/components/Clock";
-  import Functional from "../src/components/Functional";
-  import TempVar from '../src/components/TempVar'
-  export default {
+
+<script lang="ts">
+
+  import Directives from './components/Directives.vue';
+  import CustomerDirectives from './components/CustomerDirectives.vue';
+
+  export default{
     components:{
-      Clock,
-      Functional,
-      TempVar
-    },
-    data(){
-      return{
-        destroyClock: false,
-        name:"vue"
-      };
+      Directives,
+      CustomerDirectives
     }
-  };
+  }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ my-app/src/components/DirectiveIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <a-tabs>
+      <a-tab-pane key="Directives" tab="内置指令">
+        <Directives/>
+      </a-tab-pane>
+      <a-tab-pane key="CustomerDirectives" tab="自定义指令">
+        <CustomerDirectives/>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import Directives from './Directives.vue';
+import CustomerDirectives from './CustomerDirectives.vue';
+export default{
+  components:{
+    Directives,
+    CustomerDirectives
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
